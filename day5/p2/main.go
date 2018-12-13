e2129169172caefaa8ce6ad5cdd7e6a95673ef67
kmollee <admin@mongkonglee.com>
2018-12-14 01:14:55 +0800

    update p2 with goroutine

--- day5/p2/main.go
+++ day5/p2/main.go
@@ -6,7 +6,13 @@ import (
 	"io/ioutil"
 	"log"
 	"strings"
+	"sync"
 )
+
+type result struct {
+	len  int
+	word rune
+}
 
 func main() {
 	data, err := ioutil.ReadFile("../input.txt")
@@ -18,37 +24,57 @@ func main() {
 
 	polymers := string(data)
 
-	var d byte
+	// var d byte
 
 	shortestpoly := len(data)
 	var removeChar rune
 
+	poly := make(chan result)
+
+	var wg sync.WaitGroup
+
 	for c := 'A'; c <= 'Z'; c++ {
-		newpolymers := strings.Replace(polymers, string(c), "", -1)
-		newpolymers = strings.Replace(newpolymers, string(c+32), "", -1)
-		for {
-			isReactive := false
-			for i := range newpolymers[:len(newpolymers)-1] {
+		wg.Add(1)
+		go func(text string, r rune, p chan<- result) {
+			defer wg.Done()
+			newpolymers := strings.Replace(text, string(r), "", -1)
+			newpolymers = strings.Replace(newpolymers, string(r+32), "", -1)
+			var d byte
+			for {
+				isReactive := false
+				for i := range newpolymers[:len(newpolymers)-1] {
 
-				if newpolymers[i] > newpolymers[i+1] {
-					d = newpolymers[i] - newpolymers[i+1]
-				} else {
-					d = newpolymers[i+1] - newpolymers[i]
+					if newpolymers[i] > newpolymers[i+1] {
+						d = newpolymers[i] - newpolymers[i+1]
+					} else {
+						d = newpolymers[i+1] - newpolymers[i]
+					}
+
+					if d == 32 {
+						newpolymers = newpolymers[:i] + newpolymers[i+2:]
+						isReactive = true
+						break
+					}
 				}
-
-				if d == 32 {
-					newpolymers = newpolymers[:i] + newpolymers[i+2:]
-					isReactive = true
+				if !isReactive {
 					break
 				}
 			}
-			if !isReactive {
-				break
-			}
-		}
-		if len(newpolymers) < shortestpoly {
-			shortestpoly = len(newpolymers)
-			removeChar = c
+
+			p <- result{len(newpolymers), r}
+		}(polymers, c, poly)
+	}
+
+	go func() {
+		wg.Wait()
+		close(poly)
+	}()
+
+	for r := range poly {
+		fmt.Printf("%v\n", r)
+		if r.len < shortestpoly {
+			shortestpoly = r.len
+			removeChar = r.word
 		}
 	}
 
